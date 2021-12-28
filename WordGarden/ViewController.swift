8713c391a58edde5a9183aebdcbef7ee0808cdad
KFAV <kofava14@gmail.com>
2021-12-27 16:45:44 -0800

    Ch. 3.2 Word Garden - Creating the UI

--- WordGarden/ViewController.swift
+++ WordGarden/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var wordsGuessedLabel: UILabel!
+    @IBOutlet weak var wordsMissedLabel: UILabel!
+    @IBOutlet weak var wordsRemainingLabel: UILabel!
+    @IBOutlet weak var wordsInGameLabel: UILabel!
+    
+    @IBOutlet weak var wordBeingRevealLabel: UILabel!
+    @IBOutlet weak var guessedLetterField: UITextField!
+    @IBOutlet weak var guessLetterButton: UIButton!
+    @IBOutlet weak var gameStatusLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var flowerImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+    }
+    
+    
 }
 
